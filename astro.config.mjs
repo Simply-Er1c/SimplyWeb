3b897a12fde9a2804f498e4423124535d924a73b
SimplyEr1c <SimplyEr1c@protonmail.com>
2025-06-22 16:19:04 -0400

    Fixed disaster build and file paths.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://simply-er1c.github.io",
-  base: "my-repo",
+  base: "/SimplyWeb/",
   vite: {
     plugins: [tailwindcss()],
   },
