04a24077305d1ce957edfb2a95eeab163389d2bc
SimplyEr1c <SimplyEr1c@protonmail.com>
2025-06-22 14:16:39 -0400

    added more deploy files and fixes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://simply-er1c.github.io",
+  base: "my-repo",
   vite: {
-    site: "https://simply-er1c.github.io",
-    base: "my-repo",
     plugins: [tailwindcss()],
   },
 });
